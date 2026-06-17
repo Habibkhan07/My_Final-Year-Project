c201f875e331017fdb57db80f6a1dce70f5d4049
habibkhan07 <hk384856@gmail.com>
2026-06-17 16:32:31 +0500

    Added to views.py

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+
+# the router will be created here and the views will be created here and the logic will be implemented here and the test cases will be written here and more over the commit will be done here
 # Create your views here.
